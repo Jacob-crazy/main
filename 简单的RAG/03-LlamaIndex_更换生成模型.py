93e763258368eb02bb16603b94260fb5a0cacef7
Jacob-crazy <75882927+Jacob-crazy@users.noreply.github.com>
2025-03-27 16:54:33 +0800

    Add files via upload

--- 简单的RAG/03-LlamaIndex_更换生成模型.py
+++ 简单的RAG/03-LlamaIndex_更换生成模型.py
@@ -15,6 +15,13 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+embedding = HuggingFaceEmbedding(model_name="BAAI/bge-small-zh")
 llm = DeepSeek(model="deepseek-reasoner", api_key=os.getenv("DEEPSEEK_API_KEY"))
 
+documents = SimpleDirectoryReader(input_files=[r'E:\Jacob_zb\LLM\AI_Agent\文案\黑悟空\设定.txt']).load_data()
 
+index = VectorStoreIndex.from_documents(documents, embed_model=embedding)
+
+query_engine = index.as_query_engine(llm=llm)
+
+print(query_engine.query("黑神话悟空中有什么战斗工具？"))
